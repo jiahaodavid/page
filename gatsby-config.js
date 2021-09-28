7da7513ed8e44548852ba25af46bfe4af04428b4
jiahaodavid <jiahaodavid4040@gmail.com>
2021-09-28 13:02:46 -0700

    remove cloud

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,6 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-offline`,
-    `gatsby-plugin-gatsby-cloud`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
